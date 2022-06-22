0d5651bd5be8f1105182ba1ed09202ab3e0c56da
StevenL <lintanf.steven@gmail.com>
2022-06-22 23:31:32 +0200

    backend creation

---
+++ models/Thing.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const thingSchema = mongoose.Schema({
+  userid: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  usersLiked: { type: Number, required: true },
+  usersDisliked: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Thing', thingSchema);
