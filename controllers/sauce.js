4a0934622a52e0ddfe4026b320580e7e0248da75
StevenL <lintanf.steven@gmail.com>
2022-06-27 13:30:36 +0200

    fixed add sauce bug (error 400)

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,6 +1,18 @@
 const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(404).json({ error }));
+};
+
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id
@@ -39,18 +51,6 @@ exports.deleteSauce = (req, res, next) => {
             });
         })
         .catch(error => res.status(500).json({ error }));
-};
-
-exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({ error }));
-};
-
-exports.getAllSauces = (req, res, next) => {
-    Sauce.find()
-        .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(400).json({ error }));
 };
 
 exports.likeSauce = (req, res, next) => {
